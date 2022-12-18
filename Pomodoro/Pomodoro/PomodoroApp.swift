d62f1e7b983c34debee34ddcc6ec601e5fb759c5
wupangyen <72144016+wupangyen@users.noreply.github.com>
2022-12-17 18:11:30 -0600

    add viewModel and view for the app

---
+++ Pomodoro/Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Pang Yen Wu on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
