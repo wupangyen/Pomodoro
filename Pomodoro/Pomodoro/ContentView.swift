bf557759d7a3d6ad60b822e50f2c64bf7a684609
pang.yenwu <pang.yenwu@life.church>
2022-12-21 10:14:26 -0600

    Add for pause the timer The pause button functionality didn't actually pause the timer fully

--- Pomodoro/Pomodoro/ContentView.swift
+++ Pomodoro/Pomodoro/ContentView.swift
@@ -8,46 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var vm = ViewModel()
-    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private let width: Double = 250
+    
+    @StateObject private var viewModel = ViewModel()
+    
+    
+    private var countDownStartTime: String = "25:00"
+   @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
+        
         VStack {
-            Text("\(vm.time)")
-                .font(.system(size: 70, weight: .medium, design: .rounded))
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.gray, lineWidth: 4))
-                .alert("Timer done!", isPresented: $vm.showingAlert) {
-                    Button("Continue", role: .cancel) {
-                        //code
-                    }
-                }
-            Slider(value: $vm.minutes, in: 1...25, step:1)
-                .padding()
-                .frame(width: width)
-                .disabled(vm.isActive)
-                .animation(.easeInOut, value: vm.minutes)
-            HStack(spacing: 50) {
-                Button("Start") {
-                    vm.start(minutes: vm.minutes)
-                }
-                .disabled(vm.isActive)
-                
-                Button("Reset", action: vm.reset)
-                    .tint(.red)
+            Text("Time: \(viewModel.time)")
+                .padding(.top, 200)
+                .padding(.bottom, 100)
+            Button("Start") {
+                viewModel.countDownStart(minutes: viewModel.minutes)
             }
-            .frame(width: width)
+            .disabled(viewModel.isTicking)
+            
+            Button {
+                timer.upstream.connect().cancel()
+                
+            } label: {
+                Label("Stop", systemImage: "nosign")
+            }
+            .buttonStyle(.bordered)
+            
+            Button {
+                timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            } label: {
+                Label("Resume", systemImage: "play")
+            }
+            .buttonStyle(.borderedProminent)
         }
         .onReceive(timer) { _ in
-            vm.updateCountdown()
-            
+            viewModel.updateCountdown()
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Pomodoro/ViewModel/PomodoroViewModel.swift
+++ Pomodoro/ViewModel/PomodoroViewModel.swift
@@ -4,50 +4,35 @@
 //
 //  Created by Pang Yen Wu on 12/17/22.
 //
-
-import Foundation
+import SwiftUI
 
 extension ContentView {
     final class ViewModel: ObservableObject {
-        @Published var isActive = false
-        @Published var showingAlert = false
-        @Published var time: String = "5:00"
-        @Published var minutes: Float = 5.0 {
+        @Published var isTicking = false
+        @Published var time: String = "25:00"
+        @Published var minutes: Float = 25.0 {
             didSet {
                 self.time = "\(Int(minutes)):00"
             }
         }
         
         private var initialTime = 0
-        private var endDate = Date()
-        
-        func start(minutes: Float) {
-            self.initialTime = Int(minutes)
-            self.endDate = Date()
-            self.isActive = true
-            self.endDate = Calendar.current.date(byAdding: .minute, value: Int(minutes), to: endDate)!
-        }
-        
-        func reset() {
-            self.minutes = Float(initialTime)
-            self.isActive = false
-            self.time = "\(Int(minutes)):00"
-        }
+        private var countDownTime = Date()
         
         func updateCountdown() {
-            guard isActive else { return }
+            guard isTicking else { return }
             
             let now = Date()
-            let diff = endDate.timeIntervalSince1970 - now.timeIntervalSince1970
+            let timeRemaining = countDownTime.timeIntervalSince1970 - now.timeIntervalSince1970
             
-            if diff <= 0 {
-                self.isActive = false
+            if timeRemaining <= 0 {
+                
+                self.isTicking = false
                 self.time = "0.00"
-                self.showingAlert = true
                 return
             }
             
-            let date = Date(timeIntervalSince1970: diff)
+            let date = Date(timeIntervalSince1970: timeRemaining)
             let calendar = Calendar.current
             let minutes = calendar.component(.minute, from: date)
             let seconds = calendar.component(.second, from: date)
@@ -56,6 +41,14 @@ extension ContentView {
             self.time = String(format: "%d:%02d", minutes, seconds)
         }
         
+        func countDownStart(minutes: Float) {
+            self.initialTime = Int(minutes)
+            self.countDownTime = Date()
+            self.isTicking = true
+            self.countDownTime = Calendar.current.date(byAdding: .minute, value: Int(minutes), to: countDownTime)!
+        }
     }
 }
+
+
 
